2bfb0ff0764c259d508462e1f351e2dae13af400
Dinesh <dk6360159@gmail.com>
2026-08-24 15:45:04 +0530

    redis integrated

--- app/core/services/redis/redis.py
+++ app/core/services/redis/redis.py
@@ -6,8 +6,8 @@ from app.core.settings import get_settings
 settings=get_settings()
 
 redist_client=redis.Redis(
-    host=settings.REDIS_HOST,
-    port=settings.REDIS_PORT,
-    db=settings.REDIS_DB,
+    host=settings.redis_host,
+    port=settings.redis_port,
+    db=settings.redis_db,
     decode_responses=True
 )
